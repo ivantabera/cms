2e964cc6c94c0af5dafe26836af42d311bb51da5
ivanTabera <ivantabera19@gmail.com>
2020-06-12 17:17:47 -0500

    se creo el hook para inicio de sesion

--- src/components/content/login/Login.js
+++ src/components/content/login/Login.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Login(){
 
+    /* Hook para iniciar sesion */
+    const [administradores, iniciarSesion] = useState({
+        usuario:"",
+        password:""
+    });
+
+    /* Capturamos cambios del formulario para ejecutar la funcion del hook */
+    const cambiaForm = (e) => {
+
+        iniciarSesion({
+            ...administradores,
+            [e.target.name]:e.target.value
+        })
+        
+    }
+
+    /* Ejecutamos el submit */
+    const login = e => {
+        e.preventDefault();
+    }
+
+    /* Retornamos la vista */
     return(
 
         <div className="hold-transition login-page">
@@ -18,7 +40,7 @@ export default function Login(){
 
                         <p className="login-box-msg">Inicia sesión</p>
 
-                        <form>
+                        <form onChange={cambiaForm} onSubmit={login}>
 
                             <div className="input-group mb-3">
 
