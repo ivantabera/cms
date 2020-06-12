ee99d165532e4b9314acd066ec952234315be10a
ivanTabera <ivantabera19@gmail.com>
2020-06-12 13:02:00 -0500

    se crearon los componentes de Sidebar, Administradores, Footer y se agregaron a la app js, por otro lado tambien se agrego el paquete para las rutas de react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header/Header'
+import Header from './components/header/Header';
+import Sidebar from './components/sidebar/Sidebar';
+import Administradores from './components/content/administradores/Administradores';
+import Footer from './components/footer/Footer';
 
 
 export default function App() {
@@ -9,6 +12,9 @@ export default function App() {
       <div className="wrapper">
 
         <Header/>
+        <Sidebar/>
+        <Administradores/>
+        <Footer/>
 
       </div>
     </div>
