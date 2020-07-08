8e20308946181aabc5a8fa346604183d2d91e4e9
ivanTabera <ivantabera19@gmail.com>
2020-07-08 18:51:04 -0500

    se realizo peticion Get para cada componente y mostrar la informacion de cada uno en la vista

--- src/components/content/slide/Slide.js
+++ src/components/content/slide/Slide.js
@@ -7,78 +7,97 @@ import 'datatables.net-responsive';
 
 export default function Slide(){
 
-    const Slide01 = `${rutaAPI}/mostrar-img-slide/447.jpg`;
+    /* Disparamos peticion getData */
+    const dataSlide = async() => {
+        
+        /* Obteniendo la informacion de la peticion GET */
+        const getSlides = await getData();
 
-    /* Creamo el dataSet para hacer dinamicos los datos de la tabla */
-    const dataSet = [
-        [1, Slide01, "Lorem, ipsum", "Lorem ipsum dolor sit amet, consectetur adipisicing elit", "5ed8386d5c83432a58524701"],
-        [2, Slide01, "Lorem, ipsum 2", "Lorem ipsum dolor sit amet, consectetur adipisicing elit 2", "4ed8386d5c83432a58524701"]
-    ];
+        /* Declaramos el dataSet para hacer dinamicos los datos de la tabla  */
+        /* Se declara la variable para generar el arreglo */
+        const dataSet = [];
 
-    /* Ejecutamos datatable */
-    $(document).ready( function () {
-        $('.table').DataTable({
+        /* Recorreomos los datos del dataset creado y lo convertimos en el arreglo que necesitamos */
+        getSlides.data.forEach((currentValue, index)=>{
+            console.log('currentValue', currentValue)
+            
+            dataSet[index] = [(index+1),
+                                currentValue.imagen,
+                                currentValue.titulo,
+                                currentValue.descripcion,
+                                currentValue._id];
+        })
 
-            data: dataSet,
-            columns: [
-                { title: "#" },
-                { title: "Imagen", 
-                    render: function (data) {
-                        return `<img src="${data}" style="width:320px" />`
-                    } 
-                },
-                { title: "Titutlo"},
-                { title: "Descripción"},
-                { title: "Acciones",
-                    render: function(data){
-                        return `
-                            <a href="#">
-                                <svg style="color:black; background:orange; border-radius:20%; padding:8px; width:35px; line-height:35px; text-align:center; "  
-                                aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pencil-alt" class="svg-inline--fa fa-pencil-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M497.9 142.1l-46.1 46.1c-4.7 4.7-12.3 4.7-17 0l-111-111c-4.7-4.7-4.7-12.3 0-17l46.1-46.1c18.7-18.7 49.1-18.7 67.9 0l60.1 60.1c18.8 18.7 18.8 49.1 0 67.9zM284.2 99.8L21.6 362.4.4 483.9c-2.9 16.4 11.4 30.6 27.8 27.8l121.5-21.3 262.6-262.6c4.7-4.7 4.7-12.3 0-17l-111-111c-4.8-4.7-12.4-4.7-17.1 0zM124.1 339.9c-5.5-5.5-5.5-14.3 0-19.8l154-154c5.5-5.5 14.3-5.5 19.8 0s5.5 14.3 0 19.8l-154 154c-5.5 5.5-14.3 5.5-19.8 0zM88 424h48v36.3l-64.5 11.3-31.1-31.1L51.7 376H88v48z"></path></svg>
-                            </a>
+        /* Comprobando que los datos vengan correctos */
+        //console.log('dataSet', dataSet)
 
-                            <a href="#">
-                                <svg style="color:black; background:red; border-radius:20%; padding:8px; width:35px; line-height:35px; text-align:center; "  
-                                aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" class="svg-inline--fa fa-trash fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path></svg
-                            </a>
-                        `
+        /* Ejecutamos datatable */
+        $(document).ready( function () {
+            $('.table').DataTable({
+
+                data: dataSet,
+                columns: [
+                    { title: "#" },
+                    { title: "Imagen", 
+                        render: function (data) {
+                            return `<img src="${rutaAPI}/mostrar-img-slide/${data}" style="width:320px" />`
+                        } 
+                    },
+                    { title: "Titutlo"},
+                    { title: "Descripción"},
+                    { title: "Acciones",
+                        render: function(data){
+                            return `
+                                <a href="#">
+                                    <svg style="color:black; background:orange; border-radius:20%; padding:8px; width:35px; line-height:35px; text-align:center; "  
+                                    aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pencil-alt" class="svg-inline--fa fa-pencil-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M497.9 142.1l-46.1 46.1c-4.7 4.7-12.3 4.7-17 0l-111-111c-4.7-4.7-4.7-12.3 0-17l46.1-46.1c18.7-18.7 49.1-18.7 67.9 0l60.1 60.1c18.8 18.7 18.8 49.1 0 67.9zM284.2 99.8L21.6 362.4.4 483.9c-2.9 16.4 11.4 30.6 27.8 27.8l121.5-21.3 262.6-262.6c4.7-4.7 4.7-12.3 0-17l-111-111c-4.8-4.7-12.4-4.7-17.1 0zM124.1 339.9c-5.5-5.5-5.5-14.3 0-19.8l154-154c5.5-5.5 14.3-5.5 19.8 0s5.5 14.3 0 19.8l-154 154c-5.5 5.5-14.3 5.5-19.8 0zM88 424h48v36.3l-64.5 11.3-31.1-31.1L51.7 376H88v48z"></path></svg>
+                                </a>
+
+                                <a href="#">
+                                    <svg style="color:black; background:red; border-radius:20%; padding:8px; width:35px; line-height:35px; text-align:center; "  
+                                    aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" class="svg-inline--fa fa-trash fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path></svg
+                                </a>
+                            `
+                        }
+                    }
+                ],
+
+                "language" : {
+                    "sProcessing":     "Procesando...",
+                    "sLengthMenu":     "Mostrar _MENU_ registros",
+                    "sZeroRecords":    "No se encontraron resultados",
+                    "sEmptyTable":     "Ningún dato disponible en esta tabla",
+                    "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+                    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+                    "sInfoPostFix":    "",
+                    "sSearch":         "Buscar:",
+                    "sUrl":            "",
+                    "sInfoThousands":  ",",
+                    "sLoadingRecords": "Cargando...",
+                    "oPaginate": {
+                        "sFirst":    "Primero",
+                        "sLast":     "Último",
+                        "sNext":     "Siguiente",
+                        "sPrevious": "Anterior"
+                    },
+                    "oAria": {
+                        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                    },
+                    "buttons": {
+                        "copy": "Copiar",
+                        "colvis": "Visibilidad"
                     }
                 }
-            ],
+            });
+        } );
+    }
 
-            "language" : {
-                "sProcessing":     "Procesando...",
-                "sLengthMenu":     "Mostrar _MENU_ registros",
-                "sZeroRecords":    "No se encontraron resultados",
-                "sEmptyTable":     "Ningún dato disponible en esta tabla",
-                "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-                "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-                "sInfoPostFix":    "",
-                "sSearch":         "Buscar:",
-                "sUrl":            "",
-                "sInfoThousands":  ",",
-                "sLoadingRecords": "Cargando...",
-                "oPaginate": {
-                    "sFirst":    "Primero",
-                    "sLast":     "Último",
-                    "sNext":     "Siguiente",
-                    "sPrevious": "Anterior"
-                },
-                "oAria": {
-                    "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                },
-                "buttons": {
-                    "copy": "Copiar",
-                    "colvis": "Visibilidad"
-                }
-            }
-        });
-    } );
+    /* Ejecutamos dataSlide */
+    dataSlide();
 
-    
-
+    /* Retornamos la vista del componente */
     return(
 
 
@@ -131,4 +150,31 @@ export default function Slide(){
 
 
     )
+}
+
+/* PETICION GET PARA SLIDE */
+const getData = () => {
+
+    /* Ruta a la cual accederemos para mostrar la informacion del slide */
+    const url = `${rutaAPI}/mostrar-slide`;
+    /* Validamos el Token del localstorage y saber que esta logueado */
+    const token = localStorage.getItem("ACCESS_TOKEN");
+
+    const params = {
+
+        method:"GET",
+        headers: {
+            "Authorization" : token,
+            "Content-type" : "application/json"
+        }
+    }
+
+    return fetch(url, params).then(response=>{
+        return response.json();
+    }).then(result=>{
+        return result;
+    }).catch(err=>{
+        return err.json;
+    });
+
 }
